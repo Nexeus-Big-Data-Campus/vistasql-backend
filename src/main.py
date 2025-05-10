5f19baf57d81994cd18287b52298b2a50e4da2e9
zaton81 <zaton81@hotmail.com>
2025-05-10 17:10:04 +0200

    add UserCreate

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 import os
 from src.crud.user_crud import add_user
+from src.dto.user import UserCreate
 
 app = FastAPI()
 
@@ -15,14 +16,14 @@ def on_startup():
     SQLModel.metadata.create_all(engine)
 
 @app.post("/sign-in")
-def register_user(name: str, email: str, password: str):
+def register_user(user: UserCreate):
     #llamamos a la función add_user para registrar el usuario
-    new_user = add_user(name, email, password)
+    new_user = add_user(user.name, user.email, user.password)
     if new_user is None:
         raise HTTPException(status_code=400, detail="El email ya está registrado")
     
     # Generar un token JWT
-    token = token = create_token(email=new_user.email, name=new_user.name)
+    token = create_token(email=new_user.email, name=new_user.name)
 
     return {"access_token": token, "token_type": "bearer"}
 
--- src/models/user.py
+++ src/models/user.py
@@ -5,8 +5,12 @@ import os
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 
-# recuperamos la clave secreta del entorno
+# recuperamos la clave secreta del entorno, le he añadido una validación y un casteo a str para evitar el error 500
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+JWT_SECRET_KEY = str(JWT_SECRET_KEY)
+
+if not JWT_SECRET_KEY:
+    raise ValueError("JWT_SECRET_KEY no está definido en las variables de entorno")
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
@@ -21,5 +25,5 @@ def create_token(email, name):
         "name": name,
         "exp": datetime.utcnow() + timedelta(hours=24)  # Token válido por 24 horas
     }
-    token = jwt.encode(token_data, JWT_SECRET_KEY, algorithm="HS256")
+    token = jwt.encode(token_data, JWT_SECRET_KEY, algorithm="HS256") 
     return token
