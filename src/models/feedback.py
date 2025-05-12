383807f407f245f62508cf9c96c8e7d16a5fbbee
Robert Salazar <robertond69@gmail.com>
2025-05-12 15:37:08 +0200

    message_type and message

--- src/models/feedback.py
+++ src/models/feedback.py
@@ -2,7 +2,6 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from enum import Enum
 
-
 class MessageType (str, Enum):
     bug = "bug"
     feedback = "feedback"
@@ -10,4 +9,6 @@ class MessageType (str, Enum):
 class Feedback(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     user_id: str = Field(foreign_key="user.id")
+    message_type: MessageType
+    message: str
     
