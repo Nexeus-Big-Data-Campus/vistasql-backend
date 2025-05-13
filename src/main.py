e6e27798625471560a41eaa0d2541999e834d967
Javier Jimenez <165967978+RelatifVision@users.noreply.github.com>
2025-05-13 17:54:16 +0200

    Modificamos la función de logout, restauramos add_feedback, eliminamos relacionadas con user_action

--- src/main.py
+++ src/main.py
@@ -4,11 +4,11 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import SQLModel, Session
 from typing import Annotated
 from src.db import engine
-from src.crud import get_user_by_email, create_user
+from src.crud import get_user_by_email, create_user, create_feedback
 from src.security import create_jwt_token
-from src.dto import UserCreate
+from src.dto import UserCreate, FeedbackCreate
 from datetime import datetime
-from user import User, UserSession
+from models.user import User, UserSession
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
 dosc_url = "/docs" if ENVIRONMENT != "prod" else None
@@ -32,32 +32,41 @@ def register_user(user: UserCreate, session: Annotated[Session, Depends(get_sess
     if new_user is None:
         raise HTTPException(status_code=400, detail="Email already exsists")
     
-    token = new_user.get_jtw_token()
+    token = new_user.get_jwt_token()
     return {"access_token": token, "token_type": "bearer"}
 
 @app.post("/sign-out")
-def logout_user(user_id: int, session: Annotated[Session, Depends(get_session)]):
+def logout_user(
+    session_id: int,
+    session: Annotated[Session, Depends(get_session)]
+):
     user_session = session.query(UserSession).filter(
-        UserSession.user_id == user_id, UserSession.end_time == None
+        UserSession.id == session_id,
+        UserSession.end_time == None
     ).first()
 
     if user_session:
         user_session.end_time = datetime.now(datetime.timezone.utc)
-        user_session.duration = int((user_session.end_time - user_session.start_time).total_seconds())
+        duration = user_session.end_time - user_session.start_time
+        user_session.duration = int(duration.total_seconds())
         session.commit()
+        return {"message": "Session closed successfully"}
+    else:
+        raise HTTPException(
+            status_code=404,
+            detail="Session not found or already closed"
+        )
 
-    return {"message": "User logged out successfully"}
 
-
+@app.post("/feedback")
+def add_feedback(
+    feedback: FeedbackCreate,
+    session: Annotated[Session, Depends(get_session)],
+):
+    return create_feedback(session, feedback)
 
 @app.get("/user-sessions/{user_id}")
 def get_user_sessions(user_id: int, session: Annotated[Session, Depends(get_session)]):
     #Obtener las sesiones del usuario
     sessions = session.query(UserSession).filter(UserSession.user_id == user_id).all()
     return sessions
-
-@app.get("/user-actions/{user_id}")
-def get_user_actions(user_id: int, session: Annotated[Session, Depends(get_session)]):
-    # Obtener las acciones del usuario
-    actions = session.query(UserAction).filter(UserAction.user_id == user_id).all()
-    return actions
