e399d2b0a3686563f0a3160c20bbfa56ebe1b133
Jordi Ferrer <jferrerbeltran@gmail.com>
2025-05-16 13:33:00 +0200

    Add login routes and session table

--- src/main.py
+++ src/main.py
@@ -1,43 +1,25 @@
+from contextlib import asynccontextmanager
 import os
-from fastapi import FastAPI, Depends, HTTPException, Request, Security
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from sqlmodel import SQLModel, Session
-from typing import Annotated
+from fastapi import FastAPI
+from sqlmodel import SQLModel
 from src.db import engine
-from src.crud import create_user, create_feedback
-from src.dto import UserCreate, FeedbackCreate
+from fastapi.middleware.cors import CORSMiddleware
 from src.middleware.auth_middleware import auth_middleware
-
+from src.routes import auth, feedback, session
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "dev")
 dosc_url = "/docs" if ENVIRONMENT != "prod" else None
 app = FastAPI(docs_url=dosc_url, redoc_url=None)
-security = HTTPBearer()
 
 app.middleware("http")(auth_middleware)
+# TODO: Allow production url on prod environment
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:3000"], allow_credentials=["*"], allow_methods=["*"], allow_headers=["*"])
 
-@app.on_event("startup")
-def on_startup():
-
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     SQLModel.metadata.create_all(engine)
+    yield
 
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-@app.post("/sign-in")
-def register_user(user: UserCreate, session: Annotated[Session, Depends(get_session)]):
-    new_user = create_user(session, user)
-    if new_user is None:
-        raise HTTPException(status_code=400, detail="Email already exsists")
-
-    token = new_user.get_jwt_token()
-    return {"access_token": token, "token_type": "bearer"}
-
-@app.post("/feedback")
-def add_feedback(
-    feedback: FeedbackCreate,
-    session: Annotated[Session, Depends(get_session)],
-    credentials: HTTPAuthorizationCredentials = Security(security),
-):
-    return create_feedback(session, feedback)
+app.include_router(auth.router)
+app.include_router(feedback.router, prefix="/feedback")
+app.include_router(session.router, prefix="/session")
--- src/models/user.py
+++ src/models/user.py
@@ -1,7 +1,13 @@
 import uuid
 from sqlmodel import SQLModel, Field
 from typing import Optional
-from src.security import create_jwt_token
+from src.security import create_jwt_token, verify_password
+from datetime import datetime
+from enum import Enum
+
+class Role(str, Enum):
+    client = "client"
+    admin = "admin"
 
 class User(SQLModel, table=True):
     id: Optional[str] = Field(
@@ -9,17 +15,24 @@ class User(SQLModel, table=True):
         primary_key=True,
         index=True,
     )
-    
     name: str
     email: str = Field(index=True, unique=True)
     password: str
+    role: Role = Field(default=Role.client)  
+    created_at: datetime = Field(default_factory=lambda: datetime.now(datetime.timezone.utc))
     
-
     def get_jwt_token(self):
         return create_jwt_token({
             'id': self.id,
             'name': self.name,
             'email': self.email,
         })
-        
     
+    def verify_password(self, password: str) -> bool:
+        return verify_password(password, self.password)
+        
+class UserSession(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: int = Field(foreign_key="user.id")  
+    start_time: datetime = Field(default_factory=lambda: datetime.now(datetime.timezone.utc))
+    end_time: Optional[datetime] = None
