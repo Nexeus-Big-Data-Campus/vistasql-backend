9b6268a454376fa893057d0b1896228714a5af17
Javier Jimenez <165967978+RelatifVision@users.noreply.github.com>
2025-06-07 14:35:48 +0200

    "Corregir errores de src/middleware/middleware_auth"

--- src/middleware/auth_middleware.py
+++ src/middleware/auth_middleware.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Depends
 from jwt import decode, exceptions
 from src.security.security import JWT_SECRET_KEY, decode_jwt_token
 from starlette.responses import JSONResponse
@@ -10,21 +10,25 @@ async def auth_middleware(request: Request, call_next):
     if request.url.path in OPEN_URLS:
         return await call_next(request)
 
-    # Obtener el token del encabezado Authorization
-    auth_header = request.headers.get("Authorization")
+    user = get_user_from_auth_header(request.headers)
+    response = await call_next(request)
+    return response
+    
+
+def get_user_from_auth_header(headers):
+    auth_header = headers.get("Authorization")
     if not auth_header or not auth_header.startswith("Bearer "):
         return JSONResponse(status_code=401, content={"detail":"Token no proporcionado o inválido"})
         
     token = auth_header.split(" ")[1]  # Extraer el token después de "Bearer"
     try:
-        # Decodificar el token JWT
-        payload = decode_jwt_token(token)
-        request.state.user = payload  # Guarda los datos del usuario en el estado de la solicitud
+        return decode_jwt_token(token)
     except exceptions.ExpiredSignatureError:
         return JSONResponse(status_code=401, detail="El token ha expirado")
     except exceptions.DecodeError:
         return JSONResponse(status_code=401, detail="Token inválido")
 
-    # Continuar con la solicitud
-    response = await call_next(request)
-    return response
+def admin_required(user=Depends(get_user_from_auth_header)):
+    if user.role != "admin":
+        raise HTTPException(status_code=403, detail="Not authorized")
+    return user
