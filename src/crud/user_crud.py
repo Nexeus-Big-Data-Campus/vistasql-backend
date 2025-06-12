c927b299ab3ccb22e210deab1f66dcd31b0cb86f
zaton81 <zaton81@hotmail.com>
2025-06-12 14:33:39 +0200

    feat: change password min lenght

--- src/crud/user_crud.py
+++ src/crud/user_crud.py
@@ -3,6 +3,7 @@ from typing import Optional
 from src.models import User
 from src.dto import UserCreate
 from src.security import get_password_hash
+from fastapi import HTTPException
 
 
 def get_user_by_email(session: Session, email: str) -> Optional[User]:
@@ -10,6 +11,8 @@ def get_user_by_email(session: Session, email: str) -> Optional[User]:
     return session.exec(statement).first()
 
 def create_user(session: Session, user_in: UserCreate) -> Optional[User]:
+    if len(user_in.password) < 8:
+        raise HTTPException(status_code=400, detail="La contraseña debe tener al menos 8 caracteres")
     existing_user = get_user_by_email(session, user_in.email)
     if existing_user:
         return None
